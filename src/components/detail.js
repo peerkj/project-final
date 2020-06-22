06cd63e6c0bd752486dcb9e8fc28b0a2cd7bcff7
jse6146 <jse6146@naver.com>
2020-06-22 20:51:36 +0900

    zz
    디테일

--- src/components/detail.js
+++ src/components/detail.js
@@ -7,6 +7,17 @@ import Info from "@material-ui/icons/InfoOutlined";
 @inject((stores) => ({
     all: stores.detail.all,
     getRecipe: stores.detail.getRecipe,
+    comp_list: stores.detail.comp_list,
+    ing_list: stores.detail.ing_list,
+    step_list: stores.detail.step_list,
+    nickname: stores.detail.nickname,
+    profile: stores.detail.profile,
+
+    comp_img: stores.detail.comp_img,
+    changeComp: stores.detail.changeComp,
+    sliderR: stores.detail.sliderR,
+    sliderL: stores.detail.sliderL,
+    sliderP: stores.detail.sliderP,
 }))
 
 @observer
@@ -14,33 +25,95 @@ class Detail extends Component {
 
     componentWillMount = () => {
         this.props.getRecipe();
+        this.props.changeComp();
 
     };
     render() {
 
         const {
             all,
+            comp_list,
+            ing_list,
+            step_list,
+            nickname,
+            profile,
+            comp_img,
+
+            sliderR,
+            sliderL,
+            sliderP,
+            changeComp,
         } = this.props;
 
+        //완성사진
+        const compphoto_list = comp_list.map(i => {
+            return (
+                <div><img src={`http://localhost:9000/acorn/image/recipe/${i}`} alt="" /> </div>
+            )
+        })
+
+        //주재료
+        const main = ing_list.map(i => {
+            return (
+                <div>
+
+                    {i.sort === '주재료' ?
+                        i.ingre_name : ""
+                    }{i.sort === '주재료' ?
+                        <Info /> : ""
+                    }&nbsp;
+                    {i.sort === '주재료' ?
+                        i.quantity : ""
+                    }
+                </div>
+            )
+        })
+
+        //부재료
+        const sub = ing_list.map(i => {
+            return (
+                <div>
+
+                    {i.sort === '부재료' ?
+                        i.ingre_name : ""
+                    }{i.sort === '부재료' ?
+                        <Info /> : ""
+                    }&nbsp;
+                    {i.sort === '부재료' ?
+                        i.quantity : ""
+                    }
+                </div>
+            )
+        })
+
+        //조리순서
+        const step = step_list.map((i, num) => {
+            return (
+                <div>
+                    <div className="stepnc">
+                        <div className="stepnum">{num + 1}</div>
+                        <p style={{ float: "left" }}><b>{i.content}</b></p><br />
+                    </div>
+                    <img src={`http://localhost:9000/acorn/image/recipe/${i.photo}`} alt="" />
+                </div>
+            )
+        })
 
         //<img src={`http://localhost:9000/acorn/image/recipe/${all.repre_photo}`} alt="" />
-        let l = all.comp_photo.split(',');
 
-        const comp_list = l.map(img => {
-            return (<div><img src={`http://localhost:9000/acorn/image/recipe/${img}`} alt="" /> </div>);
-        });
 
         return (
             <div>
+                {/* Default */}
                 <div style={{ width: "100%", height: "300px", border: "1px solid black" }}>
-                    {comp_list}
+                    <img src={`http://localhost:9000/acorn/image/recipe/${all.repre_photo}`} alt="" />
                 </div>
                 <div>
                     <div style={{ position: "relative", width: "100%", border: "1px solid black" }}>
-                        <img className="de_profile" src="img/basic_user.png" alt="" />
+                        <img className="de_profile" src={`http://localhost:9000/acorn/image/profile/${profile}`} alt="" />
                         <br />
                         <br />
-                        <p style={{ textAlign: "center" }}>비와이프</p>
+                        <p style={{ textAlign: "center" }}>{nickname}</p>
                         <p style={{ textAlign: "center" }}><b>{all.subject}</b></p>
                         <p style={{ textAlign: "center" }}>"{all.summary}"</p>
                         <div className="ptdGroup">
@@ -52,29 +125,31 @@ class Detail extends Component {
                     <div style={{ width: "100%", border: "1px solid black" }}>
                         재료<br />
                         <b>주재료</b><br />
-                        주재료1 <Info />
-                        <hr />
-                        주재료2 <Info />
+                        {main}
                         <hr />
                         <br />
                         <b>부재료</b><br />
-                        부재료1 <Info />
-                        <hr />
-                        부재료2 <Info />
+                        {sub}
                         <hr />
 
                         <b>조리순서</b><br />
+                        {step}
                         <br />
                         <br />
                         <b>완성사진</b><br />
                         <div>
-
+                            <img src={`http://localhost:9000/acorn/image/recipe/${comp_img}`} alt="" />
+                            <button onClick={sliderL}>&lt;</button>
+                            <button onClick={sliderP}>일시정지</button>
+                            <button onClick={changeComp}>재생</button>
+                            <button onClick={sliderR}>&gt;</button>
                         </div><br /><br />
                         <b>요리 Tip!</b><br />
                         {all.tip}
                     </div>
 
                 </div>
+                {/* Default */}
             </div>
         );
     }
