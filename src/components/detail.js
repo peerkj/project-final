ba6836014d39dccfb23f5e123c9130c1841386a0
LEEYEJI08 <63325398+LEEYEJI08@users.noreply.github.com>
2020-06-26 12:26:23 +0900

    댓글 수정

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect, useRef } from "react";
+import styled from 'styled-components';
 import { inject, observer } from "mobx-react";
 import { Link } from "react-router-dom";
 import Comment from "./comment";
@@ -8,7 +9,7 @@ import {
   Share, Close, People, Timer, Star, ChatBubbleOutline,
   Bookmark, FavoriteBorderSharp, FavoriteSharp, ViewList, ViewCarousel, Subject,
   KeyboardArrowRight, KeyboardArrowLeft, PlayArrow, Pause,
-  ExpandLess, BookmarkBorderSharp
+  ExpandLess, BookmarkBorderSharp, BorderColor
 } from "@material-ui/icons";
 import {
   Button, Dialog, TextField, DialogContent, DialogTitle,
@@ -45,6 +46,9 @@ import queryString from "query-string";
 
   checkscr: stores.detail.checkscr,
   Scrap: stores.detail.Scrap,
+  setValue: stores.comment.setValue,
+  handleOpen: stores.comment.handleOpen,
+
 
   //step 모드
   changeStep: stores.detail.changeStep,
@@ -88,6 +92,8 @@ class Detail extends Component {
       //댓글모달
       comment_open,
       handleComment,
+      setValue,
+      handleOpen,
 
       checkjoa,
       Joayo,
@@ -190,253 +196,298 @@ class Detail extends Component {
       );
     };
 
-    return (
-      <div>
-        {/* Default */}
-        {/* 썸네일 */}
-        <div
-          style={{ width: "100%", height: "270px" }}
-        >
-          <div className="detailThumbnail">
-            <div className="centered">
-              <img
-                className="detailThumbnailImg"
-                src={`http://localhost:9000/acorn/image/recipe/${all.repre_photo}`}
-                alt=""
-              />
+    //위로가기
+    const UpArea = styled.div`
+      position: relative;
+    `;
+
+    const UpWrap = styled.div`  
+        position: fixed;
+        left: "330px",
+        top: "605px",
+        z-index: 1;
+        width: "30px",
+        height: "30px",
+        transition: .4s ease;
+        &.hide {
+            transform: translateY(-80px);
+        }
+    `;
+
+    const PageUp = () => {
+      const [hide, setHide] = useState(false);
+      const [pageY, setPageY] = useState(0);
+      const documentRef = useRef(document);
+
+      const handleUpScroll = () => {
+        const { pageYOffset } = window;
+        const deltaY = pageYOffset - pageY;
+        const hide = pageYOffset !== 0 && deltaY >= 0;
+        setHide(hide);
+        setPageY(pageYOffset);
+      }
+
+      useEffect(() => {
+        documentRef.current.addEventListener('scroll', handleUpScroll);
+        return () => documentRef.current.removeEventListener('scroll', handleUpScroll);
+      }, [pageY]);
+
+      return (
+        <div>
+          {/* Default */}
+          {/* 썸네일 */}
+          <div
+            style={{ width: "100%", height: "270px" }}
+          >
+            <div className="detailThumbnail">
+              <div className="centered">
+                <img
+                  className="detailThumbnailImg"
+                  src={`http://localhost:9000/acorn/image/recipe/${all.repre_photo}`}
+                  alt=""
+                />
+              </div>
             </div>
           </div>
-        </div>
-        {/* 글 정보 */}
-        <div>
-          <div
-            style={{
-              position: "relative",
-              width: "100%",
-            }}
-          >
-            <center>
-              <img
-                className="de_profile"
-                src={`http://localhost:9000/acorn/image/profile/${all.profile}`}
-                alt=""
-              />
-              <p style={{ fontWeight: "500", fontSize: "10pt" }}>
-                {all.nickname}
-              </p>
+          {/* 글 정보 */}
+          <div>
+            <div
+              style={{
+                position: "relative",
+                width: "100%",
+              }}
+            >
+              <center>
+                <img
+                  className="de_profile"
+                  src={`http://localhost:9000/acorn/image/profile/${all.profile}`}
+                  alt=""
+                />
+                <p style={{ fontWeight: "500", fontSize: "10pt" }}>
+                  {all.nickname}
+                </p>
 
-              {/* 요리 제목 */}
-              <p id="detail_titletext">
-                {all.subject}
-              </p>
+                {/* 요리 제목 */}
+                <p id="detail_titletext">
+                  {all.subject}
+                </p>
 
-              {/* 요리 설명 */}
-              <p>
-                <img src="/img/quote1.png" alt="" width="16px" />
-                <span id="detail_subtext">
-                  {all.summary}
-                </span>
-                <img src="/img/quote2.png" alt="" width="16px" />
-              </p>
-              <br />
-              <div className="ptdGroup">
-                <div className="ptd">
-                  <People color="disabled" /><br />
-                  <span className="texts">{all.portion}</span>
+                {/* 요리 설명 */}
+                <p>
+                  <img src="/img/quote1.png" alt="" width="16px" />
+                  <span id="detail_subtext">
+                    {all.summary}
+                  </span>
+                  <img src="/img/quote2.png" alt="" width="16px" />
+                </p>
+                <br />
+                <div className="ptdGroup">
+                  <div className="ptd">
+                    <People color="disabled" /><br />
+                    <span className="texts">{all.portion}</span>
+                  </div>
+                  <div className="ptd">
+                    <Timer color="disabled" /><br />
+                    <span className="texts">{all.time}</span>
+                  </div>
+                  <div className="ptd">
+                    <Star color="disabled" /><br />
+                    <span className="texts">{all.difficult}</span>
+                  </div>
                 </div>
-                <div className="ptd">
-                  <Timer color="disabled" /><br />
-                  <span className="texts">{all.time}</span>
-                </div>
-                <div className="ptd">
-                  <Star color="disabled" /><br />
-                  <span className="texts">{all.difficult}</span>
-                </div>
-              </div>
-              <div className="ptd2Group">
-                <div className="ptd2" onClick={handleShare} >
-                  <Share /><br />
+                <div className="ptd2Group">
+                  <div className="ptd2" onClick={handleShare} >
+                    <Share /><br />
                   URL 복사
                 </div>
-                <div className="ptd2" onClick={Joayo}>
-                  {checkjoa === 0 ? <FavoriteBorderSharp /> : <FavoriteSharp style={{ color: "#db555a" }} />}<br />
-                  {checkjoa === 0 ? "좋아요" : "좋아요 취소"}
-                </div>
-                <div className="ptd2" onClick={Scrap}>
-                  {checkscr === 0 ? <BookmarkBorderSharp /> : <Bookmark style={{ color: "#db555a" }} />}<br />
-                  {checkscr === 0 ? "스크랩" : "스크랩 취소"}
-                </div>
-                <div className="ptd2" onClick={handleComment}>
-                  <ChatBubbleOutline /><br />
+                  <div className="ptd2" onClick={Joayo}>
+                    {checkjoa === 0 ? <FavoriteBorderSharp /> : <FavoriteSharp style={{ color: "#db555a" }} />}<br />
+                    {checkjoa === 0 ? "좋아요" : "좋아요 취소"}
+                  </div>
+                  <div className="ptd2" onClick={Scrap}>
+                    {checkscr === 0 ? <BookmarkBorderSharp /> : <Bookmark style={{ color: "#db555a" }} />}<br />
+                    {checkscr === 0 ? "스크랩" : "스크랩 취소"}
+                  </div>
+                  <div className="ptd2" onClick={handleComment}>
+                    <ChatBubbleOutline /><br />
                   댓글 {c_count}개
                 </div>
-              </div>
-            </center>
-          </div>
-          <hr className="detailLine" />
-          <div style={{ width: "100%" }}>
-            <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>재료
+                </div>
+              </center>
+            </div>
+            <hr className="detailLine" />
+            <div style={{ width: "100%" }}>
+              <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>재료
               <span class="detailIngreTitleText">Ingredients</span>
-            </p>
-            <p className="detailMainTitle">[주재료]</p>
-            {main}
-            <br />
-            <p className="detailMainTitle">[부재료]</p>
-            {sub}
-            <hr className="detailLine" />
-            <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>조리 순서
-              <span class="detailIngreTitleText">Steps</span>
-            </p>
-            <div className="detailStepCate">
-              <div className="category"
-                onClick={() => {
-                  //기본 0
-                  changeStep(0);
-                }}>
-                <ViewList /><br />
-              </div>
-              <div className="category"
-                onClick={() => {
-                  //슬라이더 2번
-                  changeStep(2);
-                }}>
-                <ViewCarousel /><br />
-              </div>
-              <div className="category"
-                onClick={() => {
-                  //글만 1번
-                  changeStep(1);
-                }}>
-                <Subject /><br />
-              </div>
-            </div>
-            <br /><br /><br />
-            <div>
-              {step_state === 0 ? step1 : step_state === 1 ? step2 : step3()}
-            </div>
-
-            <hr className="detailLine" />
-            <div>
-              <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>완성 사진
-              <span class="detailIngreTitleText">Finish</span>
               </p>
-
-              <div className="stepImgWrapper">
-                <div className="centered">
-                  <img
-                    src={`http://localhost:9000/acorn/image/recipe/${comp_img}`}
-                    alt=""
-                  />
+              <p className="detailMainTitle">[주재료]</p>
+              {main}
+              <br />
+              <p className="detailMainTitle">[부재료]</p>
+              {sub}
+              <hr className="detailLine" />
+              <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>조리 순서
+              <span class="detailIngreTitleText">Steps</span>
+              </p>
+              <div className="detailStepCate">
+                <div className="category"
+                  onClick={() => {
+                    //기본 0
+                    changeStep(0);
+                  }}>
+                  <ViewList /><br />
+                </div>
+                <div className="category"
+                  onClick={() => {
+                    //슬라이더 2번
+                    changeStep(2);
+                  }}>
+                  <ViewCarousel /><br />
+                </div>
+                <div className="category"
+                  onClick={() => {
+                    //글만 1번
+                    changeStep(1);
+                  }}>
+                  <Subject /><br />
                 </div>
               </div>
-              <div style={{ textAlign: "center" }}>
-                <KeyboardArrowLeft onClick={sliderL} />&ensp;
+              <br /><br /><br />
+              <div>
+                {step_state === 0 ? step1 : step_state === 1 ? step2 : step3()}
+              </div>
+
+              <hr className="detailLine" />
+              <div>
+                <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>완성 사진
+              <span class="detailIngreTitleText">Finish</span>
+                </p>
+
+                <div className="stepImgWrapper">
+                  <div className="centered">
+                    <img
+                      src={`http://localhost:9000/acorn/image/recipe/${comp_img}`}
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div style={{ textAlign: "center" }}>
+                  <KeyboardArrowLeft onClick={sliderL} />&ensp;
                 <Pause onClick={sliderP} />&ensp;
                 <PlayArrow onClick={changeComp} />&ensp;
                 <KeyboardArrowRight onClick={sliderR} />
+                </div>
               </div>
-            </div>
-            <br />
-            <br />
-            <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>
-              <img src="/img/star.jpg" alt="" width="30px" style={{ verticalAlign: "middle" }} />
-              <span style={{ verticalAlign: "middle" }}>요리 Tip!</span>
-            </p>
-            <div id="tipBox">{all.tip}</div>
-            <center style={{ margin: "50px 0 80px" }}>
-              <Button
-                onClick={() => {
-                  history.push("/recipe");
-                }}
-                variant="outlined"
-                color="black"
-                component="span"
-                className={useStyles.button}
-              >
-                목록
+              <br />
+              <br />
+              <p style={{ fontSize: "16pt", fontWeight: "500", marginLeft: "5px" }}>
+                <img src="/img/star.jpg" alt="" width="30px" style={{ verticalAlign: "middle" }} />
+                <span style={{ verticalAlign: "middle" }}>요리 Tip!</span>
+              </p>
+              <div id="tipBox">{all.tip}</div>
+              <center style={{ margin: "50px 0 80px" }}>
+                <Button
+                  onClick={() => {
+                    history.push("/recipe");
+                  }}
+                  variant="outlined"
+                  color="black"
+                  component="span"
+                  className={useStyles.button}
+                >
+                  목록
               </Button>
-            </center>
+              </center>
+            </div>
           </div>
-        </div>
-        {/* Default */}
+          {/* Default */}
 
-        {/* 위로가기 */}
-        <Link
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
-        >
-          <ExpandLess
-            style={{
-              position: "fixed",
-              left: "330px",
-              top: "605px",
-              width: "30px",
-              height: "30px",
-              border: "1px solid #575757",
-              backgroundColor: "#ffffff",
-              opacity: "0.8",
-              color: "#000000",
+          {/* 위로가기 */}
+
+          <UpArea>
+            <UpWrap className={hide && 'hide'}>
+            </UpWrap>
+          </UpArea>
+          <Link
+            onClick={() => {
+              window.scrollTo(0, 0);
             }}
-          />
-        </Link>
+          >
+            <ExpandLess
+              style={{
+                width: "30px",
+                height: "30px",
+                border: "1px solid #575757",
+                backgroundColor: "#ffffff",
+                opacity: "0.8",
+                color: "#000000",
+              }}
+            />
+          </Link>
 
-        {/* 공유모달 */}
-        <div>
-          <Dialog open={modal_open} onClose={handleShare}>
-            <DialogTitle id="form-dialog-title">
-              <span style={{ fontSize: "12pt" }}>URL 복사하기</span>
-              <IconButton edge="end" onClick={handleShare} aria-label="close">
-                <Close style={{ marginLeft: "130px", marginTop: "-10px" }} />
-              </IconButton>
-            </DialogTitle>
+          {/* 공유모달 */}
+          <div>
+            <Dialog open={modal_open} onClose={handleShare}>
+              <DialogTitle id="form-dialog-title">
+                <span style={{ fontSize: "12pt" }}>URL 복사하기</span>
+                <IconButton edge="end" onClick={handleShare} aria-label="close">
+                  <Close style={{ marginLeft: "130px", marginTop: "-10px" }} />
+                </IconButton>
+              </DialogTitle>
 
-            <DialogContent>
-              <TextField
-                value={url}
-                readOnly
-                fullWidth
-                variant="filled"
-                size="small"
-              />
-              <CopyToClipboard text={url} onCopy={onCopy}>
-                <center>
-                  <Button style={{ margin: "20px 0" }} variant="outlined">
-                    복사
+              <DialogContent>
+                <TextField
+                  value={url}
+                  readOnly
+                  fullWidth
+                  variant="filled"
+                  size="small"
+                />
+                <CopyToClipboard text={url} onCopy={onCopy}>
+                  <center>
+                    <Button style={{ margin: "20px 0" }} variant="outlined">
+                      복사
                   </Button>
-                </center>
-              </CopyToClipboard>
+                  </center>
+                </CopyToClipboard>
+              </DialogContent>
+            </Dialog>
+          </div>
+          {/* 공유모달 */}
+
+          {/* 댓글모달 */}
+          <Dialog fullScreen open={comment_open} onClose={handleComment}
+          >
+            <AppBar style={{ height: "50px", width: "100%", backgroundColor: "#002060", marginLeft: "-84px" }}>
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="close"
+                  style={{ marginTop: "-5px" }}
+                >
+                  <Close
+                    onClick={handleComment} />
+                  <BorderColor
+                    onClick={() => {
+                      setValue();
+                      handleOpen();
+                    }}
+                    style={{ marginLeft: "295px" }}
+                  />
+                </IconButton>
+              </Toolbar>
+            </AppBar>
+            <br />
+            <br />
+            <DialogContent>
+              <Comment />
             </DialogContent>
           </Dialog>
-        </div>
-        {/* 공유모달 */}
-
-        {/* 댓글모달 */}
-        <Dialog fullScreen open={comment_open} onClose={handleComment}
-        >
-          <AppBar style={{ height: "50px", width: "100%", backgroundColor: "#002060", marginLeft: "-84px" }}>
-            <Toolbar>
-              <IconButton
-                edge="start"
-                color="inherit"
-                onClick={handleComment}
-                aria-label="close"
-                style={{ marginTop: "-5px" }}
-              >
-                <Close />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-          <br />
-          <br />
-          <DialogContent>
-            <Comment />
-          </DialogContent>
-        </Dialog>
-        {/* 댓글모달 */}
-      </div >
-    );
+          {/* 댓글모달 */}
+        </div >
+      );
+    }
   }
-}
 
-export default Detail;
+  export default Detail;
